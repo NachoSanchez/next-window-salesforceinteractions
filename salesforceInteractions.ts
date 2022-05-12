add1e0cf5fb72424486fbf85c0b38418401ff522
NachoSanchez <m.ign.sanchez@gmail.com>
2022-05-12 18:16:23 -0300

    improvments

--- salesforceInteractions.ts
+++ salesforceInteractions.ts
@@ -1,35 +1,23 @@
-const button = document.getElementById("button");
-function onMount() {
-  console.log('other script :', window.SalesforceInteractions.sendEvent({
 
+function onClick() {
+  window.SalesforceInteractions.sendEvent({
     interaction: {
-
         name: window.SalesforceInteractions.CatalogObjectInteractionName.ViewCatalogObject,
-
         catalogObject: {
-
             type: "Category",
-
             id: () => window.location.search.split("=")[1],
-
             attributes: {
-
                 url: () => window.location.href,
-
                 name: () => window.location.search.split("=")[1],
-
             }
-
         }
-
     },
-
     user: { identities: { emailAddress: "ignacio.sanchez@devsutd.com" } }
-
-}));
+  });
 }
 
-button?.addEventListener('click', onMount);
+const button = document.getElementById("button");
+button?.addEventListener('click', onClick);
 
 export default function LogSalesforceActions () {
   return null
